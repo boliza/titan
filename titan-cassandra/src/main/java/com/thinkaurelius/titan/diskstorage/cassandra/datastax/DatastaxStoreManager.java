29f8c90fa469523216815a1141d7dcd6e59f7d6d
Ranger Tsao <cao.zhifu@gmail.com>
2014-08-15 21:02:44 +0800

    datastax policy options

--- titan-cassandra/src/main/java/com/thinkaurelius/titan/diskstorage/cassandra/datastax/DatastaxStoreManager.java
+++ titan-cassandra/src/main/java/com/thinkaurelius/titan/diskstorage/cassandra/datastax/DatastaxStoreManager.java
@@ -1,7 +1,9 @@
 package com.thinkaurelius.titan.diskstorage.cassandra.datastax;
 
-import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.STORAGE_PORT;
+import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.BASIC_METRICS;
+import static com.thinkaurelius.titan.graphdb.configuration.GraphDatabaseConfiguration.METRICS_JMX_ENABLED;
 
+import java.lang.reflect.Constructor;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.util.List;
@@ -19,12 +21,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.HostDistance;
 import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.PlainTextAuthProvider;
+import com.datastax.driver.core.PoolingOptions;
+import com.datastax.driver.core.ProtocolOptions;
+import com.datastax.driver.core.QueryOptions;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.SSLOptions;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.SocketOptions;
 import com.datastax.driver.core.TableMetadata;
+import com.datastax.driver.core.policies.LoadBalancingPolicy;
+import com.datastax.driver.core.policies.LoggingRetryPolicy;
+import com.datastax.driver.core.policies.ReconnectionPolicy;
+import com.datastax.driver.core.policies.RetryPolicy;
 import com.thinkaurelius.titan.diskstorage.BackendException;
 import com.thinkaurelius.titan.diskstorage.PermanentBackendException;
 import com.thinkaurelius.titan.diskstorage.StaticBuffer;
@@ -44,11 +55,20 @@ public class DatastaxStoreManager extends AbstractCassandraStoreManager {
 
     private static final Logger log = LoggerFactory.getLogger(DatastaxStoreManager.class);
 
+    /**
+     * CQL for get partitioner
+     */
     private static final String SELECT_LOCAL = "SELECT partitioner FROM system.local WHERE key='local'";
-    private static final String CREATE_KEYSPACE = "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = {'class':'%s'%s}";
-    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS %s (key blob,column1 blob,value blob,PRIMARY KEY (key,column1)) WITH comment = 'column family %s' ";
 
-    private static final int NATIVE_TRANSPORT_PORT = 9042;
+    /**
+     * CQL for create keyspace
+     */
+    private static final String CREATE_KEYSPACE = "CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION = {'class':'%s'%s}";
+
+    /**
+     * CQL for create table
+     */
+    private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS %s (key blob,column1 blob,value blob,PRIMARY KEY (key,column1)) WITH comment = 'column family %s' ";
 
     //Config for datastax
     public static final ConfigNamespace DATASTAX_NS = new ConfigNamespace(CASSANDRA_NS, "datastax", "datastax-specific Cassandra options");
@@ -58,19 +78,211 @@ public class DatastaxStoreManager extends AbstractCassandraStoreManager {
      * <p/>
      */
     public static final ConfigOption<String> CLUSTER_NAME =
-            new ConfigOption<String>(DATASTAX_NS, "cluster-name",
+            new ConfigOption<String>(
+                    DATASTAX_NS,
+                    "cluster-name",
                     "Default name for the Cassandra cluster",
-                    ConfigOption.Type.MASKABLE, "Titan Cluster");
+                    ConfigOption.Type.MASKABLE,
+                    "Titan Cluster");
+
+    //protocol config
+    public static final ConfigOption<Integer> PROTOCOL_VERSION =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "protocol-version",
+                    "The native protocol version to use." +
+                            "The driver supports both version 1 and 2 of the native protocol. Version 2" +
+                            "of the protocol has more features and should be preferred, but it is only" +
+                            "supported by Cassandra 2.0 and above, so you will have to use version 1 with" +
+                            "Cassandra 1.2 nodes." +
+                            "By default the driver will 'auto-detect'(-1) which protocol version it can use" +
+                            "when connecting to the first node",
+                    ConfigOption.Type.FIXED,
+                    2);
+
+    public static final ConfigOption<ProtocolOptions.Compression> PROTOCOL_COMPRESSION =
+            new ConfigOption<ProtocolOptions.Compression>(
+                    DATASTAX_NS,
+                    "protocol-compression",
+                    "What kind of compression to use" +
+                            "when sending data to a node: either no compression or snappy." +
+                            "Snappycompression isoptimized for high speeds and reasonable compression",
+                    ConfigOption.Type.FIXED,
+                    ProtocolOptions.Compression.NONE
+            );
+
+    //pooling config
+    public static final ConfigOption<HostDistance> HOST_DISTANCE =
+            new ConfigOption<HostDistance>(
+                    DATASTAX_NS,
+                    "host-distance",
+                    "The distance assigned to an host influences how many connections the driver" +
+                            "maintains towards this host",
+                    ConfigOption.Type.MASKABLE,
+                    HostDistance.LOCAL
+            );
+
+    public static final ConfigOption<Integer> CORE_CONNECTIONS =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "core-connections",
+                    "The core number of connections per host." +
+                            "2 for HostDistance.LOCAL, 1 for HostDistance.REMOTE",
+                    ConfigOption.Type.MASKABLE,
+                    Integer.class
+            );
+
+    public static final ConfigOption<Integer> MAX_CONNECTIONS =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "max-connections",
+                    "The maximum number of connections per host." +
+                            "8 for HostDistance.LOCAL, 2 for HostDistance.REMOTE",
+                    ConfigOption.Type.MASKABLE,
+                    Integer.class
+            );
+
+    public static final ConfigOption<Integer> MIN_REQUESTS =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "min-requests",
+                    "The number of simultaneous requests on a connection below" +
+                            "which connections in excess are reclaimed",
+                    ConfigOption.Type.MASKABLE,
+                    25
+            );
+
+    public static final ConfigOption<Integer> MAX_REQUESTS =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "max-requests",
+                    "The number of simultaneous requests on all connections " +
+                            "to an host after which more connections are created",
+                    ConfigOption.Type.MASKABLE,
+                    100
+            );
+
+    //socket config
+    public static final ConfigOption<Integer> CONNECT_TIMEOUT =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "connect-timeout",
+                    "The connect timeout in milliseconds for the underlying Netty channel",
+                    ConfigOption.Type.MASKABLE,
+                    5000
+            );
+
+    public static final ConfigOption<Integer> READ_TIMEOUT =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "read-timeout",
+                    "The read timeout in milliseconds for the underlying Netty channel.",
+                    ConfigOption.Type.MASKABLE,
+                    12000
+            );
+
+    public static final ConfigOption<Boolean> KEEP_ALIVE =
+            new ConfigOption<Boolean>(
+                    DATASTAX_NS,
+                    "keep-alive",
+                    "",
+                    ConfigOption.Type.MASKABLE,
+                    false
+            );
+
+    public static final ConfigOption<Boolean> REUSE_ADDRESS =
+            new ConfigOption<Boolean>(
+                    DATASTAX_NS,
+                    "reuse-address",
+                    "Whether to allow the same port to be bound to multiple times",
+                    ConfigOption.Type.MASKABLE,
+                    false
+            );
+
+    public static final ConfigOption<Boolean> TCP_NO_DELAY =
+            new ConfigOption<Boolean>(
+                    DATASTAX_NS,
+                    "tcp-no-delay",
+                    "Disables Nagle's algorithm on the underlying socket",
+                    ConfigOption.Type.MASKABLE,
+                    false
+            );
+
+    public static final ConfigOption<Integer> SO_LINGER =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "so-linger",
+                    "When specified, disables the immediate return from a call to close() on a TCP socket",
+                    ConfigOption.Type.MASKABLE,
+                    Integer.class
+            );
+
+    public static final ConfigOption<Integer> RECEIVE_BUFFER_SIZE =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "receive-buffer-size",
+                    "A hint on the size of the buffer used to send data",
+                    ConfigOption.Type.MASKABLE,
+                    Integer.class
+            );
+
+    public static final ConfigOption<Integer> SEND_BUFFER_SIZE =
+            new ConfigOption<Integer>(
+                    DATASTAX_NS,
+                    "send-buffer-size",
+                    "A hint on the size of the buffer used to receive data",
+                    ConfigOption.Type.MASKABLE,
+                    Integer.class
+            );
+
+    //policies config
+    public static final ConfigOption<String> RETRY_POLICY =
+            new ConfigOption<String>(
+                    DATASTAX_NS,
+                    "retry-policy",
+                    "Datastax's retry policy implementation",
+                    ConfigOption.Type.MASKABLE,
+                    "DefaultRetryPolicy");
+
+    public static final ConfigOption<String> RECONNECTION_POLICY =
+            new ConfigOption<String>(
+                    DATASTAX_NS,
+                    "reconnection-policy",
+                    "Datastax's reconnection policy implementation with configuration parameters",
+                    ConfigOption.Type.MASKABLE,
+                    "com.datastax.driver.core.policies.ExponentialReconnectionPolicy,1000,5000");
+
+    public static final ConfigOption<String> LOAD_BALANCING_POLICY =
+            new ConfigOption<String>(
+                    DATASTAX_NS,
+                    "load-balancing-policy",
+                    "Datastax's reconnection policy implementation with configuration parameters",
+                    ConfigOption.Type.MASKABLE,
+                    "com.datastax.driver.core.policies.RoundRobinPolicy");
 
     private final Cluster cluster;
     private final Map<String, DatastaxKeyColumnValueStore> openStores;
 
     public DatastaxStoreManager(Configuration config) throws BackendException {
         super(config);
+
+        RetryPolicy retryPolicy = getRetryPolicy(config.get(RETRY_POLICY));
+        ReconnectionPolicy reconnectionPolicy = getReconnectionPolicy(config.get(RECONNECTION_POLICY));
+        LoadBalancingPolicy loadBalancingPolicy = getLoadBalancingPolicy(config.get(LOAD_BALANCING_POLICY));
+
         Cluster.Builder builder = Cluster.builder();
         builder.addContactPoints(StringUtils.join(hostnames, ","));
-        builder.withPort(config.has(STORAGE_PORT) ? config.get(STORAGE_PORT) : NATIVE_TRANSPORT_PORT);
+        builder.withPort(port);//TODO must set value
         builder.withClusterName(config.get(CLUSTER_NAME));
+
+        //protocol options
+        if (config.has(PROTOCOL_VERSION)) {
+            builder.withProtocolVersion(config.get(PROTOCOL_VERSION));
+        }
+
+        if (config.has(PROTOCOL_COMPRESSION)) {
+            builder.withCompression(config.get(PROTOCOL_COMPRESSION));
+        }
 
         if (hasAuthentication()) {
             builder.withAuthProvider(new PlainTextAuthProvider(username, password));
@@ -90,6 +302,57 @@ public class DatastaxStoreManager extends AbstractCassandraStoreManager {
                 throw new TemporaryBackendException(e);
             }
         }
+
+        //pooling options
+        PoolingOptions poolingOptions = new PoolingOptions();
+        HostDistance distance = config.get(HOST_DISTANCE);
+        if (distance != HostDistance.IGNORED) {
+            int coreConnections = config.has(CORE_CONNECTIONS) ?
+                    config.get(CORE_CONNECTIONS) :
+                    (distance == HostDistance.LOCAL ? 2 : 1);
+            poolingOptions.setCoreConnectionsPerHost(distance, coreConnections);
+
+            int maxConnections = config.has(MAX_CONNECTIONS) ?
+                    config.get(MAX_CONNECTIONS) :
+                    (distance == HostDistance.LOCAL ? 8 : 2);
+            poolingOptions.setMaxConnectionsPerHost(distance, maxConnections);
+
+            poolingOptions.setMaxSimultaneousRequestsPerConnectionThreshold(distance, config.get(MAX_REQUESTS));
+            poolingOptions.setMinSimultaneousRequestsPerConnectionThreshold(distance, config.get(MIN_REQUESTS));
+        }
+        builder.withPoolingOptions(poolingOptions);
+
+        //socket options
+        SocketOptions socketOptions = new SocketOptions();
+        socketOptions.setKeepAlive(config.get(KEEP_ALIVE));
+        socketOptions.setReuseAddress(config.get(REUSE_ADDRESS));
+        socketOptions.setTcpNoDelay(config.get(TCP_NO_DELAY));
+        socketOptions.setConnectTimeoutMillis(config.get(CONNECT_TIMEOUT));
+        socketOptions.setReadTimeoutMillis(config.get(READ_TIMEOUT));
+        if (config.has(SO_LINGER)) {
+            socketOptions.setSoLinger(config.get(SO_LINGER));
+        }
+        if (config.has(RECEIVE_BUFFER_SIZE)) {
+            socketOptions.setSoLinger(config.get(RECEIVE_BUFFER_SIZE));
+        }
+        if (config.has(SEND_BUFFER_SIZE)) {
+            socketOptions.setSoLinger(config.get(SEND_BUFFER_SIZE));
+        }
+
+        //metrics options
+        if (!config.get(BASIC_METRICS)) {//disable metrics
+            builder.withoutMetrics();
+        } else if (!config.get(METRICS_JMX_ENABLED)) {//disable Jmx report
+            builder.withoutJMXReporting();
+        }
+
+        //query options
+        builder.withQueryOptions(new QueryOptions().setFetchSize(pageSize));
+
+        //policy options
+        builder.withRetryPolicy(retryPolicy);
+        builder.withReconnectionPolicy(reconnectionPolicy);
+        builder.withLoadBalancingPolicy(loadBalancingPolicy);
 
         cluster = builder.build();
         ensureKeyspaceExists();
@@ -222,6 +485,104 @@ public class DatastaxStoreManager extends AbstractCassandraStoreManager {
         } finally {
             session.close();
         }
+    }
+
+    enum DatastaxRetryPolicy {
+
+        DefaultRetryPolicy {
+            @Override
+            public RetryPolicy getRetryPolicy(DatastaxRetryPolicy policy) {
+                return com.datastax.driver.core.policies.DefaultRetryPolicy.INSTANCE;
+            }
+        },
+        DowngradingConsistencyRetryPolicy {
+            @Override
+            public RetryPolicy getRetryPolicy(DatastaxRetryPolicy policy) {
+                return com.datastax.driver.core.policies.DowngradingConsistencyRetryPolicy.INSTANCE;
+            }
+        },
+        FallthroughRetryPolicy {
+            @Override
+            public RetryPolicy getRetryPolicy(DatastaxRetryPolicy policy) {
+                return com.datastax.driver.core.policies.FallthroughRetryPolicy.INSTANCE;
+            }
+        },
+        LoggingRetryPolicy {
+            @Override
+            public RetryPolicy getRetryPolicy(DatastaxRetryPolicy policy) {
+                return new LoggingRetryPolicy(policy.getRetryPolicy(null));
+            }
+        };
+
+        public abstract RetryPolicy getRetryPolicy(DatastaxRetryPolicy policy);
+
+    }
+
+    private RetryPolicy getRetryPolicy(String policyString) {
+        if (policyString.indexOf(",") > 0) {
+            String[] polices = policyString.split(",");
+            if (!polices[0].equals("LoggingRetryPolicy") || polices[1].equals("LoggingRetryPolicy")) {
+                throw new IllegalArgumentException("config " + RETRY_POLICY.getName() + " is wrong when it set to LoggingRetryPolicy");
+            }
+            return DatastaxRetryPolicy.valueOf(polices[0]).getRetryPolicy(DatastaxRetryPolicy.valueOf(polices[1]));
+        }
+        return DatastaxRetryPolicy.valueOf(policyString).getRetryPolicy(null);
+    }
+
+    //TODO the load Balancing Policy is very difficult to build
+    private LoadBalancingPolicy getLoadBalancingPolicy(String policyString) {
+        return null;
+    }
+
+    private ReconnectionPolicy getReconnectionPolicy(String policyString) throws BackendException {
+        String[] tokens = policyString.split(",");
+        String policyClassName = tokens[0];
+        int argCount = tokens.length - 1;
+        Integer[] args = new Integer[argCount];
+        for (int i = 1; i < tokens.length; i++) {
+            args[i - 1] = Integer.valueOf(tokens[i]);
+        }
+
+        try {
+            ReconnectionPolicy rp = instantiate(policyClassName, args, policyString);
+            log.debug("Instantiated ReconnectionPolicy object {} from config string \"{}\"", rp, policyString);
+            return rp;
+        } catch (Exception e) {
+            throw new PermanentBackendException("Failed to instantiate Datastax Reconnection Policy class", e);
+        }
+    }
+
+    //init policy
+    @SuppressWarnings("unchecked")
+    private static <V> V instantiate(String policyClassName, Integer[] args, String raw) throws Exception {
+        for (Constructor<?> con : Class.forName(policyClassName).getConstructors()) {
+            Class<?>[] parameterTypes = con.getParameterTypes();
+
+            // match constructor by number of arguments first
+            if (args.length != parameterTypes.length)
+                continue;
+
+            // check if the constructor parameter types are compatible with argument types (which are integer)
+            // note that we allow long.class arguments too because integer is cast to long by runtime.
+            boolean intsOrLongs = true;
+            for (Class<?> pc : parameterTypes) {
+                if (!pc.equals(int.class) && !pc.equals(long.class)) {
+                    intsOrLongs = false;
+                    break;
+                }
+            }
+
+            // we found a constructor with required number of parameters but times didn't match, let's carry on
+            if (!intsOrLongs)
+                continue;
+
+            if (log.isDebugEnabled())
+                log.debug("About to instantiate class {} with {} arguments", con.toString(), args.length);
+
+            return (V) con.newInstance(args);
+        }
+
+        throw new Exception("Failed to identify a class matching the config string \"" + raw + "\"");
     }
 
 }
